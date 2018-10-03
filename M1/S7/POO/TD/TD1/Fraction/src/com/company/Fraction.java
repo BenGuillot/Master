9780c954393cd20547c1ce80a0d963eddf612df5
BenGuillot <ben.guyot.guillot@gmail.com>
2018-10-03 16:46:14 +0200

    Fraction Workflow

---
+++ M1/S7/POO/TD/TD1/Fraction/src/com/company/Fraction.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Fraction {
+    private int num;
+    private int denum;
+
+    public Fraction(int pnum, int pdenum){
+        this.num = pnum;
+        this.denum = pdenum;
+    }
+
+    public Fraction (int pnum){
+        this.num = pnum;
+        this.denum = 1;
+    }
+
+    public Fraction(){
+        this.num = 0;
+        this.denum = 1;
+    }
+
+    public int getDenum() {
+        return denum;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public double FloatRes() {
+        double res;
+        return res=(double)this.num/this.denum;
+    }
+
+    @Override
+    public String toString() {
+        return this.num+" / "+this.denum+" = "+this.FloatRes();
+    }
+
+    public boolean isEqual(Fraction pFraction){
+        int pFDenum = pFraction.getDenum();
+        int pFNum = pFraction.getNum();
+        if(pFDenum % this.denum == 0 && pFNum % this.num == 0)
+            return true;
+        return false;
+    }
+
+    public String BiggerOrLower (Fraction pFraction){
+        if(pFraction.FloatRes() > this.FloatRes())
+            return pFraction.toString() + " is bigger than "+ this.toString();
+        else if (pFraction.FloatRes() < this.FloatRes())
+            return pFraction.toString() + " is lower than "+ this.toString();
+        else
+            return pFraction.toString() + " is equal to "+ this.toString();
+    }
+}
---
+++ M1/S7/POO/TD/TD1/Fraction/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(1,3);
+        Fraction f2 = new Fraction(2, 6);
+        System.out.print(f1.BiggerOrLower(f2));
+        /*boolean isIt = f1.isEqual(f2);
+        System.out.print(isIt);*/
+    }
+}
