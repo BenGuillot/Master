7f7619f47f5efc79c9bffcef4abab9f9f273571e
BenGuillot <ben.guyot.guillot@gmail.com>
2019-02-15 17:30:27 +0100

    update

---
+++ M1/S8/ProgGL&P/TD2/Exo4.1/Exo/src/main/java/Max/Exo/AbstractGroup.java
@@ -0,0 +1,10 @@
+package Max.Exo;
+
+public abstract class AbstractGroup {
+	
+		public AbstractGroup() {}
+		
+		public abstract void affichageProfondeur();
+		public abstract void affichageLargeur();
+	
+}
---
+++ M1/S8/ProgGL&P/TD2/Exo4.2CalculetRPN/CalculetRPN/test/Ctest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Ctest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
